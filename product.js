13da93b44ec4fccded0f0f0a3e00d01843e5b50b
Kaushik-hub77 <kaushiknagaraj77@gmail.com>
2024-03-09 23:28:22 +0530

    upd

--- product.js
+++ product.js
@@ -1,12 +1,37 @@
-var sidenav= document.getElementById("sidenavbar")
-var menuicon= document.getElementById("menuicon")
-var closenav= document.getElementById("closenav")
+var sidenav = document.getElementById("sidenavbar")
+var menuicon = document.getElementById("menuicon")
+var closenav = document.getElementById("closenav")
 
-menuicon.addEventListener("click",function(){
-    sidenav.style.right=0
+menuicon.addEventListener("click", function () {
+    sidenav.style.right = 0
 
 })
 
-closenav.addEventListener("click",function(){
-    sidenav.style.right="-50%"
+closenav.addEventListener("click", function () {
+    sidenav.style.right = "-50%"
+})
+
+
+var productcontainer = document.getElementById("product-container")
+
+var search = document.getElementById("search")
+var productlist = productcontainer.querySelectorAll("div") 
+
+  search.addEventListener("keyup",function(){
+    var enteredValue= event.target.value.toUpperCase()
+
+
+    for(count=0; count<productlist.length; count=count+1)
+    {
+        var productlistitem= productlist[count].querySelector("h1").textContent
+
+        if(productlistitem[count].toUpperCase().indexOf(enteredValue)<0)
+        {
+             productlist[count].style.display= "none"
+        }
+
+        else{
+            productlist[count].style.display="block"
+        }
+    }
 })
